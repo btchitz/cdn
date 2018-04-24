5d09a60b026557801f3c88b9df7582017311ba11
BTCHitz <38687109+btchitz@users.noreply.github.com>
2018-04-24 06:32:04 -0300

    Add files via upload

---
+++ bootstrap_custom.js
@@ -0,0 +1,12 @@
+$(function() {		
+	
+	$('.dropdown-toggle').dropdown();
+	$('#navbar').scrollspy();
+	$('.orange').removeClass('orange').addClass('btn btn-info');
+	$('.buttonblue').removeClass('buttonblue').addClass('brown-but');
+	$('table.formt').removeClass('formt').addClass('table table-bordered table-striped');
+	$('.tooltip').removeClass('tooltip').addClass('tooltip-evo');
+	$('input[type=button]').addClass('btn btn-info');
+	$('input[type=submit]').addClass('btn btn-info');
+});
+
